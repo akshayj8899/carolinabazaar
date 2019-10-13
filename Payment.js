403410571566b209f6ae77f47a3bc6de7f9d461a
Vraj Patel <patelvap@gmail.com>
2019-10-12 21:27:41 -0400

    finished payment api with capital 1

--- Payment.js
+++ Payment.js
@@ -1,15 +1,11 @@
-       
-//template 
-fetch('http://api.reimaginebanking.com/atms?lat=38.9283&lng=-77.1753&rad=1&key=e11057297c6bb0535b4ffa00cdd87052').then(
-    response => response.json()
-    ).then(data => { 
-        console.log(data);
-    })
-     
+import fetch from 'node-fetch';
+
 //create escrow
-const createEscrow = () => {
-    createCustomer("ESCROW", "ESCROW", "ESCROW", "ESCROW", "ESCROW", "ESCROW", "ESCROW")
+//returns id of escrow - very important
+export const createEscrow = () => {
+    return createCustomer("ESCROW", "ESCROW", "ESCROW", "ESCROW", "ESCROW", "NA", "00000");
 }
+
 
 export const getEscrow = () => {
     fetch(`http://api.reimaginebanking.com/merchants/ESCROW?key=e11057297c6bb0535b4ffa00cdd87052`).then(
@@ -19,7 +15,7 @@ export const getEscrow = () => {
         });
 }
     
-export const sendMoneyToEscrow = (id, amount) => {
+export const sendMoneyToEscrow = (id, escrow_id, amount) => {
     fetch(`http://api.reimaginebanking.com/accounts/${id}/transfers?key=e11057297c6bb0535b4ffa00cdd87052`, {
         method: 'POST',
         headers: {
@@ -28,16 +24,18 @@ export const sendMoneyToEscrow = (id, amount) => {
         body: JSON.stringify ({
             "medium": "balance",
             "amount": `${amount}`,
-            "payee_id": "ESCROW",
+            "payee_id": `${escrow_id}`,
             "transaction_date": Date.toString(),
             "status": "completed",
             "description": "string"
         })
     });
+
+    
 }
 
-const sendMoneyToCustomer = (id, amount) => {
-    fetch(`http://api.reimaginebanking.com/accounts/ESCROW/transfers?key=e11057297c6bb0535b4ffa00cdd87052`, {
+export const sendMoneyToCustomer = (id, escrow_id, amount) => {
+    fetch(`http://api.reimaginebanking.com/accounts/${escrow_id}/transfers?key=e11057297c6bb0535b4ffa00cdd87052`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -53,16 +51,19 @@ const sendMoneyToCustomer = (id, amount) => {
     });
 }
 
-export const completeTransaction = (sender_id, receiver_id) => {
+export const completeTransaction = (transfer_id, receiver_id) => {
     //calls sendmoneytocustomer and sends what 
+    transaction_data = getTransaction(transfer_id);
+    sendMoneyToCustomer("ESCROW", transaction_data.amount);
 }
 
-export const cancelTransaction = (sender_id) => {
-
+export const cancelTransaction = (transfer_id) => {
+    transaction_data = getTransaction(transfer_id);
+    sendMoneyToCustomer(transaction_data.payer_id, transaction_data.amount)
 }
 
-//create customer
-const createCustomer = (firstName, lastName, street_number, street_name, city, state, zip) => {
+// returns the id of the created customer
+export const createCustomer = (firstName, lastName, street_number, street_name, city, state, zip) => {
     fetch('http://api.reimaginebanking.com/customers?key=e11057297c6bb0535b4ffa00cdd87052', {
         method: 'POST',
         headers: {
@@ -79,5 +80,5 @@ const createCustomer = (firstName, lastName, street_number, street_name, city, s
                 "zip": `${zip}`
             }
         })
-    })
+    }).then((response) => response.json()).then((result) => {return result.objectCreated._id});
 }
